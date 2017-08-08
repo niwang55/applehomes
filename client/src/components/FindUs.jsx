c1120802b471cf76a1c00571c9b19b6fd0d9686d
Nicholas Wang <nickwang55@gmail.com>
2017-08-08 10:42:54 -0700

    FindUs Changes

--- client/src/components/FindUs.jsx
+++ client/src/components/FindUs.jsx
@@ -12,6 +12,13 @@ export default class FindUs extends Component {
         <p>Apple Homes is a home sharing company under Han Realty Inc.</p>
         <p>You can find more details about us at <a href="http://hanrealty.us">www.hanrealty.us</a></p>
         <p>Office located at 207 S. First Ave, Arcadia, CA 91007</p>
+        <p>Contact us at</p>
+        <p>English</p>
+        <p>(626)215-4448</p>
+        <p>hanrealty8@gmail.com</p>
+        <p>Chinese</p>
+        <p>(909)699-2299</p>
+        <p>waterapple8899@gmail.com</p>
       </div>
     );
   }
